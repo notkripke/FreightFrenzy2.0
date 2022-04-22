c8955aa10cda5b9c580a8e78446e23ba404c379a
notkripkeHOME <cleary-ethan@el.gstboces.org>
2022-04-22 11:58:04 -0400

    Made VuforiaLocaliztionTest
    This program acts like the usual LocalizationTest, but instead of keeping track of movement with odometers, it uses the vuforia localization to produce its actual location. It will only localize when holding B, and the robot will automatically stop itself when doing do. After this, pressing A and Y simultaneously will have the robot generate a trajectory and follow it autonomously. It will use the drive's pose estimate (which is influenced by vuforia after holding B) and attempt to drive to the specified Waypoint, currently defined as the center of the field.

---
+++ teamcode/drive/opmode/Tests/VuforiaLocalizationTest.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcodeGIT.teamcode.drive.opmode.Tests;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcodeGIT.teamcode.drive.GorillabotsCentral;
+import org.firstinspires.ftc.teamcodeGIT.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcodeGIT.teamcode.drive.StandardTrackingWheelLocalizer;
+
+/*
+This is an opMode designed to test vuforia's localization capabilities. The robot will drive around, and the
+localization may be started by holding B. The robot will take this information and attempt to drive to the
+desired location (determined by Waypoint) autonomously after pressing BOTH A & Y
+*/
+
+
+@TeleOp(group = "drive")
+public class VuforiaLocalizationTest extends GorillabotsCentral {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        boolean vision = false;
+
+        final double Scan_Time = 3.00;
+
+        final Pose2d Waypoint = new Pose2d(0,0,Math.toRadians(0));
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+            if(gamepad1.b) {
+                vision = true;
+            }
+            if(!gamepad1.b){
+                vision = false;
+            }
+
+            if(vision){
+
+                drive.setWeightedDrivePower(new Pose2d(-0, 0,0));
+                drive.update();
+                sleep(150);
+                VuforiaScan(Scan_Time, vision);
+            }
+
+
+            if(!vision){
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+
+            drive.update();
+            }
+
+            if(gamepad1.a && gamepad1.y){
+                Trajectory MoveToWaypoint = drive.trajectoryBuilder(VuforiaLocalizedPose2d()).lineToLinearHeading(Waypoint).build();
+                drive.followTrajectory(MoveToWaypoint);
+            }
+
+
+
+        }
+
+
+        }
+    }
