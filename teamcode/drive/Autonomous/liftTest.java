9fb54f884ff49bfabd61399b9117cb1440d2cc11
notkripkeHOME <cleary-ethan@el.gstboces.org>
2022-05-14 20:59:02 -0400

    Put PID lift testing in liftTest
    Hopefully in the future, this sorta algorithm will help us control the lift. kP, kI, and kD will need tuning, but MAX_POWER should help keep tuning and testing safe.

--- teamcode/drive/Autonomous/liftTest.java
+++ teamcode/drive/Autonomous/liftTest.java
@@ -15,30 +15,83 @@ import org.firstinspires.ftc.teamcodeGIT.teamcode.drive.StandardTrackingWheelLoc
 /*
  * This is an example of a more complex path to really test the tuning.
  */
-@Disabled
 @Autonomous(group = "drive")
 public class liftTest extends GorillabotsCentral {// 192.168.43.1:8080/dash
     @Override
     public void runOpMode() throws InterruptedException {
         initializeComponents();
 
-        int Init_height = robot.lift.getCurrentPosition();
+        float kP = 0;
+        float kI = 0;
+        float kD = 0;
 
-        while(!isStarted() && !isStopRequested()) {
-            double s = 94 * (Math.sqrt(2) * sensors.getDistanceSideDist() +
-                    (2 * Math.sqrt(2))); //S   C   A   L   E
-            double target = Init_height + s;
-            int targetint = (int) Math.round(target);
-            telemetry.addData("Target: ", targetint);
-            telemetry.addData("Distance: ", sensors.getDistanceSideDist());
-            telemetry.addData("liftheight: ", robot.lift.getCurrentPosition());
-            telemetry.update();
-        }
+        double target;
 
+        double INIT_POS = robot.lift.getCurrentPosition();
+
+        target = INIT_POS;
+
+        double integralSum = 0;
+
+        double error = 0;
+
+        double lastError = 0;
+
+        double derivative;
+
+        boolean target_reached = true;
+
+        double output;
+
+        double MAX_SPEED = 0.4;
+
+        double CURRENT_POS = INIT_POS;
+
+        timer.reset();
 
         waitForStart();
 
-        raiseLiftTeleop(Init_height);
+        while(!isStopRequested()){
+
+            if(gamepad1.b){
+                target += 500;
+            }
+
+            CURRENT_POS = robot.lift.getCurrentPosition();
+
+            error = target-robot.lift.getCurrentPosition();
+
+            derivative = (error - lastError) / timer.seconds();
+
+            integralSum = integralSum + (error * timer.seconds());
+
+            output = (kP * error) + (kI * integralSum) + (kD * derivative);
+
+            if(output > MAX_SPEED){
+                output = MAX_SPEED;
+            }
+
+            robot.lift.setPower(output);
+
+            lastError = error;
+
+            if(((CURRENT_POS < target * .95) || CURRENT_POS > target  *1.05)){
+                target_reached = false;
+            }
+            if(((CURRENT_POS > target * 0.95) && CURRENT_POS < target * 1.05)){
+                target_reached = true;
+                robot.lift.setPower(0);
+            }
+
+            telemetry.addData("Current position: ", CURRENT_POS);
+            telemetry.addData("Target: ", target);
+            telemetry.addData("Error: ", error);
+            telemetry.addData("Output Speed: ", output);
+            telemetry.addData("Target Reached: ", target_reached);
+            telemetry.update();
+            timer.reset();
+
+        }
 
     }
 }
